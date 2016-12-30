f5a810cf8dc8a0ec40e895793d3027964b5bbd0c
minimo <minimo.mobile@gmail.com>
2016-12-30 17:52:28 +0900

    Move assets directories.

--- src/assets.js
+++ src/assets.js
@@ -12,10 +12,10 @@ phina.define("qft.Assets", {
                 case "common":
                     return {
                         image: {
-                            "player1":    "assets/actor4.png",
-                            "player2":    "assets/actor19.png",
-                            "item":       "assets/item.png",
-                            "background": "assets/pipo-bg001.jpg",
+                            "player1":    "assets/image/actor4.png",
+                            "player2":    "assets/image/actor19.png",
+                            "item":       "assets/image/item.png",
+                            "background": "assets/image/pipo-bg001.jpg",
                         },
                     };
                 default:
