2d56117860a7eb37fc1bf8531e62925492f402a8
minimo <minimo.mobile@gmail.com>
2017-01-24 19:13:07 +0900

    Update.

--- src/stage/stage1.js
+++ src/stage/stage1.js
@@ -9,20 +9,16 @@
 phina.define("qft.Stage1", {
     superClass: "qft.StageController",
 
-    //マップデータ
     tmx: null,
 
     init: function(parent, player, tmx) {
         this.superInit(parent, player, tmx);
 
-        //初期化処理
+        //初期処理
         this.add(1, function() {
             app.playBGM("stage1", true);
-            this.player.isAfterburner = true;
         });
         this.add(60, function() {
-            this.ground.tweener.clear().to({scaleX:1.0, scaleY:1.0, speed:3.0}, 300, "easeInOutCubic");
-            this.player.isAfterburner = false;
         });
     },
 });
