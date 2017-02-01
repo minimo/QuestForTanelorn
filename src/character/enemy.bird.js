5546376baf6a675d19eed3c3730f79788e4a6ec7
minimo <minimo.mobile@gmail.com>
2017-02-01 10:08:33 +0900

    Update enemy.

--- src/character/enemy.bird.js
+++ src/character/enemy.bird.js
@@ -10,7 +10,7 @@ phina.define("qft.Enemy.Bird", {
     superClass: "qft.Enemy",
 
     //ヒットポイント
-    hp: 20,
+    hp: 30,
 
     //防御力
     deffence: 10,
@@ -19,10 +19,10 @@ phina.define("qft.Enemy.Bird", {
     power: 10,
 
     //視力
-    eyesight: 128,
+    eyesight: 256,
 
     //視野角
-    viewAngle: 90,
+    viewAngle: 60,
 
     //重力加速度
     gravity: 0,
@@ -38,29 +38,42 @@ phina.define("qft.Enemy.Bird", {
         this.advanceTime = 10;
 
         this.setAnimation("walk");
+        this.advanceTime = 6;
         this.setupLifeGauge();
 
         this.direction = 0;
-        this.vx = 2;
+        this.speed = 2;
 
-        this.returnTime = 30;
+        this.returnTime = 120;
     },
 
     update: function() {
         if (this.isDead) return;
 
-        //壁に当たったら折り返し
-        if (this.checkMapCollision2(this.x-12, this.y, 5, 5)) {
-            this.vx = 2;
-            this.returnTime = 30;
-        } else if (this.checkMapCollision2(this.x+12, this.y, 5, 5)) {
-            this.vx = -2;
-            this.returnTime = 30;
+        if (this.isLookPlayer()) {
+            this.speed = 4;
+            this.returnTime = 60;
+        } else {
+            this.speed = 2;
         }
 
+        //方向決定
+        if (this.direction == 0) this.vx = this.speed;
+        if (this.direction == 180) this.vx = -this.speed;
+
+        //壁に当たったら折り返し
+        if (this.checkMapCollision2(this.x-12, this.y, 5, 5)) {
+            this.direction = 0;
+            this.returnTime = 120;
+        } else if (this.checkMapCollision2(this.x+12, this.y, 5, 5)) {
+            this.direction = 180;
+            this.returnTime = 120;
+        }
+
+        //一定時間過ぎたら折り返し
         if (this.returnTime < 0) {
-            this.vx *= -1;
-            this.returnTime = Math.randInt(30, 60);
+            (this.direction == 0)? this.direction = 180: this.direction = 0;
+            this.returnTime = Math.randint(100, 140);
         }
         this.returnTime--;
     },
@@ -68,7 +81,7 @@ phina.define("qft.Enemy.Bird", {
     setupAnimation: function() {
         this.spcialAnimation = false;
         this.frame = [];
-        this.frame["walk"] = [51, 52, 53, 52];
+        this.frame["walk"] = [63, 64, 65, 64];
         this.index = 0;
     },
 });
