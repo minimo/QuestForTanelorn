2ead728a6333b222f525bcd1c73af8a882e6b498
minimo <minimo.mobile@gmail.com>
2017-02-16 19:34:16 +0900

    Update effect.

--- src/character/effect.js
+++ src/character/effect.js
@@ -21,21 +21,22 @@ phina.define("qft.Effect", {
         this.superInit();
         this.setupAnimation(options);
 
-        this.sprite = phina.display.Sprite("effect");
+        this.sprite = phina.display.Sprite("effect").addChildTo(this);
+
+        this.frame = [0, 1, 2, 3];
+        this.index = 0;
 
         this.on('enterframe', function() {
             //アニメーション
             if (this.isAdvanceAnimation && this.time % this.advanceTime == 0) {
-                this.index = (this.index+1) % this.frame[this.nowAnimation].length;
-                if (this.frame[this.nowAnimation][this.index] == "stop") this.index--;
-                this.sprite.frameIndex = this.frame[this.nowAnimation][this.index];
+                this.index = (this.index+1) % this.frame.length;
+                if (this.frame[this.index] == "stop") this.index--;
+                this.sprite.frameIndex = this.frame[this.index];
             }
+            this.time++;
         });
     },
 
-    setupAnimation: function(options) {
-        this.spcialAnimation = false;
-        this.frame = [0, 1, 2, 3];
-        this.index = 0;
+    setAnimation: function(options) {
     },
 });
