8f8d3b84f6aa2e3b79deebbc3fa67ce9d5bed1cb
minimo <minimo.mobile@gmail.com>
2017-04-06 11:35:35 +0900

    Add player center on screen flag.

--- src/scene/mainscene.js
+++ src/scene/mainscene.js
@@ -36,6 +36,9 @@ phina.define("qft.MainScene", {
     limitWidth: false,
     limitHeight: true,
 
+    //スクリーン座標プレイヤー中央固定フラグ
+    attensionPlayer: true,
+
     init: function(options) {
         this.superInit({width: SC_W, height: SC_H});
         options = (options || {}).$safe({
@@ -235,21 +238,23 @@ phina.define("qft.MainScene", {
         }
 
         //スクリーン表示位置をプレイヤー中心になる様に調整
-        var map = this.mapLayer.map;
-        this.mapLayer.x = SC_W*0.5-this.player.x;
-        this.mapLayer.y = SC_H*0.5-this.player.y;
-        if (this.limitHeight) {
-            if (this.mapLayer.y > 0) this.mapLayer.y = 0;
-            if (this.mapLayer.y < -(map.height-SC_H)) this.mapLayer.y = -(map.height-SC_H);
-        }
-        if (this.limitWidth) {
-            if (this.mapLayer.x > 0) this.mapLayer.x = 0;
-            if (this.mapLayer.x < -(map.width-SC_W)) this.mapLayer.x = -(map.width-SC_W);
-        }
+        if (this.attensionPlayer) {
+            var map = this.mapLayer.map;
+            this.mapLayer.x = SC_W*0.5-this.player.x;
+            this.mapLayer.y = SC_H*0.5-this.player.y;
+            if (this.limitHeight) {
+                if (this.mapLayer.y > 0) this.mapLayer.y = 0;
+                if (this.mapLayer.y < -(map.height-SC_H)) this.mapLayer.y = -(map.height-SC_H);
+            }
+            if (this.limitWidth) {
+                if (this.mapLayer.x > 0) this.mapLayer.x = 0;
+                if (this.mapLayer.x < -(map.width-SC_W)) this.mapLayer.x = -(map.width-SC_W);
+            }
 
-        //スクリーン座標
-        this.screenX = -this.mapLayer.x;
-        this.screenY = -this.mapLayer.y;
+            //スクリーン座標
+            this.screenX = -this.mapLayer.x;
+            this.screenY = -this.mapLayer.y;
+        }
 
         this.time++;
     },
