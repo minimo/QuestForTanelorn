42f2410fe6ce3cad976682565315fce6095ece43
minimo <minimo.mobile@gmail.com>
2017-04-16 02:03:02 +0900

    Update practice scene flow.

--- src/scene/practicescene.js
+++ src/scene/practicescene.js
@@ -121,3 +121,31 @@ phina.define("qft.ConfigScene_Practice", {
     },
 });
 
+phina.define("qft.PracticePlatform", {
+    superClass: "phina.display.DisplayScene",
+
+    init: function(options) {
+        this.superInit();
+        this.options = (options || {}).$safe({
+            assetType: "stage2",
+            startStage: 2,
+        })
+        this.count = 0;
+    },
+
+    onenter: function() {
+        app.pushScene(qft.LoadingScene({
+            assets: qft.Assets.get(this.options),
+        }));
+    },
+
+    onresume: function() {
+        this.count++;
+        if (this.count == 1) {
+            app.pushScene(qft.MainScene({startStage: this.options.startStage, isPractice: true}));
+        }
+        if (this.count == 2){
+            this.exit();
+        }
+    }
+});
--- src/scene/sceneflow.js
+++ src/scene/sceneflow.js
@@ -43,40 +43,84 @@ phina.define("qft.SceneFlow", {
                 label: "ending",
                 className: "qft.EndingScene",
                 nextLabel: "main",
-            },            {
+            },{
+                label: "config",
+                className: "qft.ConfigScene",
+                nextLabel: "title",
+            },{
                 label: "practice",
                 className: "qft.ConfigScene_Practice",
+                nextLabel: "config",
             }],
         });
     }
 });
 
-
-phina.define("qft.PracticePlatform", {
-    superClass: "phina.display.DisplayScene",
+phina.define("qft.SceneFlow_Practice", {
+    superClass: "phina.game.ManagerScene",
 
     init: function(options) {
-        this.superInit();
-        this.options = (options || {}).$safe({
-            assetType: "stage2",
-            startStage: 2,
-        })
-        this.count = 0;
-    },
-
-    onenter: function() {
-        app.pushScene(qft.LoadingScene({
-            assets: qft.Assets.get(this.options),
-        }));
-    },
-
-    onresume: function() {
-        this.count++;
-        if (this.count == 1) {
-            app.pushScene(qft.MainScene({startStage: this.options.startStage, isPractice: true}));
-        }
-        if (this.count == 2){
-            this.exit();
-        }
+        options = options || {};
+        startLabel = options.startLabel || "start";
+        this.superInit({
+            startLabel: startLabel,
+            scenes: [{
+                label: "start",
+                className: "qft.ConfigScene_Practice",
+            },{
+                label: "stage1",
+                className: "qft.MainScene",
+                arguments: {
+                    startStage: 1,
+                    isPractice: true,
+                },
+                nextLabel: "start",
+            },{
+                label: "stage2",
+                className: "qft.LoadingScene",
+                arguments: {
+                    assetType: "stage2",
+                },
+                nextLabel: "stage2_main",
+            },{
+                label: "stage2_main",
+                className: "qft.MainScene",
+                arguments: {
+                    startStage: 2,
+                    isPractice: true,
+                },
+                nextLabel: "start",
+            },{
+                label: "stage3",
+                className: "qft.LoadingScene",
+                arguments: {
+                    assetType: "stage3",
+                },
+                nextLabel: "stage3_main",
+            },{
+                label: "stage3_main",
+                className: "qft.MainScene",
+                arguments: {
+                    startStage: 3,
+                    isPractice: true,
+                },
+                nextLabel: "start",
+            },{
+                label: "stage4",
+                className: "qft.LoadingScene",
+                arguments: {
+                    assetType: "stage4",
+                },
+                nextLabel: "stage4_main",
+            },{
+                label: "stage4_main",
+                className: "qft.MainScene",
+                arguments: {
+                    startStage: 4,
+                    isPractice: true,
+                },
+                nextLabel: "start",
+            }],
+        });
     }
 });
