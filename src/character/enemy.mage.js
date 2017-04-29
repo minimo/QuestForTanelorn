5e2eb1d86fabd042584640d8d84903069e7d8765
minimo <minimo.mobile@gmail.com>
2017-04-29 09:58:11 +0900

    Update enemy algorithm.

--- src/character/enemy.mage.js
+++ src/character/enemy.mage.js
@@ -22,7 +22,7 @@ phina.define("qft.Enemy.Mage", {
     eyesight: 256,
 
     //視野角
-    viewAngle: 90,
+    viewAngle: 270,
 
     //得点
     point: 100,
@@ -52,6 +52,7 @@ phina.define("qft.Enemy.Mage", {
         this.direction = 0;
         this.speed = 1;
         this.phase = 0;
+        this.isAttack = false;
 
         this.on('damaged', e => {
             if (e.direction == 0) this.direction = 180; else this.direction = 0;
@@ -59,26 +60,13 @@ phina.define("qft.Enemy.Mage", {
     },
 
     algorithm: function() {
+        var player = this.parentScene.player;
         var look = this.isLookPlayer();
         var distance = this.getDistancePlayer();
 
         if (this.onFloor) {
-            //崖っぷちで折り返す
-            if (this.checkMapCollision2(this.x+5, this.y+20, 5, 5) == null) {
-                this.direction = 180;
-            } else if (this.checkMapCollision2(this.x-5, this.y+20, 5, 5) == null) {
-                this.direction = 0;
-            }
-
-            //壁に当たったら折り返す
-            if (this._collision[1].hit) {
-                this.direction = 180;
-            } else if (this._collision[3].hit) {
-                this.direction = 0;
-            }
-
             //プレイヤーが近くにいたら距離を取る
-            if (this.phase == 0 && look && !this.isJump && distance < 64) {
+            if (this.phase == 0 && look && !this.isJump && distance < 128) {
                 this.flare('balloon', {pattern: "!"});
                 this.phase = 1;
                 this.speed = 3;
@@ -86,32 +74,68 @@ phina.define("qft.Enemy.Mage", {
 
             //逃げるフェーズ
             if (this.phase == 1) {
-                if (this.phase == 0 && look && !this.isJump && distance < 96) {
-                    if (this.x < this.player.x) this.diretion = 0; else this.direction = 180;
+                if (distance < 128) {
+                    if (this.x < player.x) this.direction = 180; else this.direction = 0;
                     this.speed = 3;
                 } else {
-                    if (this.x < this.player.x) this.diretion = 180; else this.direction = 0;
+                    if (this.x < player.x) this.direction = 0; else this.direction = 180;
                     this.speed = 0;
-                    this.phase = 3;
+                    this.phase = 2;
                 }
+                this.sprite.setFrameTrimming((this.level % 2) * 144, Math.floor(this.level / 2) * 128, 72, 128);
             }
 
             //攻撃フェーズ
             if (this.phase == 2) {
-                if (this.phase == 0 && look && !this.isJump && distance < 64) {
+                if (distance < 96) {
                     //プレイヤーが近づいたら逃げる
-                    this.sprite.setFrameTrimming((this.level % 2) * 144 + 72, Math.floor(this.level / 2) * 128, 72, 128);
                     this.phase = 1;
                 } else {
                     //プレイヤーが遠くにいる場合は攻撃
-                    this.sprite.setFrameTrimming((this.level % 2) * 144, Math.floor(this.level / 2) * 128, 72, 128);
-                    var b = this.parentScene.spawnEnemy(this.x, this.y, "Bullet", {explode: true});
-                    b.rotation = this.getPlayerAngle();
+                    if (this.time % 15 == 0) this.isAttack = true;
                 }
             }
 
-            //プレイヤーが見えない場合は通常フェーズ
-            if (!look) this.phase = 0;
+            //発狂モード
+            if (this.phase == 3) {
+                if (this.time % 5 == 0) this.isAttack = true;
+            }
+
+            if (this.isAttack) {
+                this.isAttack = false;
+                var b = this.parentScene.spawnEnemy(this.x, this.y, "Bullet", {explode: true});
+                b.rotation = this.getPlayerAngle() + Math.randint(-40, 40);
+                if (this.x < player.x) {
+                    this.diretion = 0;
+                    this.scaleX = 1;
+                } else {
+                    this.direction = 180;
+                    this.scaleX = -1;
+                }
+                this.sprite.setFrameTrimming((this.level % 2) * 144 + 72, Math.floor(this.level / 2) * 128, 72, 128);
+            }
+
+            //これ以上進めない場合は折り返す
+            var cantescape = false;
+            if (this._collision[1].hit || this.checkMapCollision2(this.x+5, this.y+20, 5, 5) == null) {
+                this.direction = 180;
+                if (this.phase == 1) cantescape = true;
+            } else if (this._collision[3].hit || this.checkMapCollision2(this.x-5, this.y+20, 5, 5) == null) {
+                this.direction = 0;
+                if (this.phase == 1) cantescape = true;
+            }
+            //逃げられない場合は発狂モード
+            if (cantescape) {
+                this.phase = 3;
+                this.speed = 0;
+            }
+
+            //プレイヤーが離れたら通常フェーズ
+            if (distance > 128 + 64) {
+                this.sprite.setFrameTrimming((this.level % 2) * 144, Math.floor(this.level / 2) * 128, 72, 128);
+                this.phase = 0;
+                this.speed = 1;
+            }
         }
 
         if (this.onFloor || this.isJump) {
