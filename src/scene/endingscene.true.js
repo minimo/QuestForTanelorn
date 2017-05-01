0db22b7951377bd98f788fc2d658857e2cdd46f0
minimo <minimo.mobile@gmail.com>
2017-05-01 17:50:23 +0900

    Add true ending scene.

---
+++ src/scene/endingscene.true.js
@@ -0,0 +1,48 @@
+/*
+ *  endingcene.true.js
+ *  2017/05/01
+ *  @auther minimo  
+ *  This Program is MIT license.
+ *
+ */
+
+//トゥルーエンディング
+phina.define("qft.EndingScene.true", {
+    superClass: "phina.display.DisplayScene",
+    
+    init: function(parentScene) {
+        this.superInit({width: SC_W, height: SC_H});
+        this.parentScene = parentScene;
+
+        //バックグラウンド
+        var param = {
+            width:SC_W,
+            height:SC_H,
+            fill: "rgba(0,0,0,1)",
+            stroke: "rgba(0,0,0,1)",
+            backgroundColor: 'transparent',
+        };
+        this.bg = phina.display.RectangleShape(param)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+        this.bg.tweener.clear()
+            .set({alpha: 0})
+            .to({alpha: 0.5}, 1000);
+
+        app.playBGM("gameover", false);
+
+        this.time = 0;
+    },
+
+    update: function(app) {
+        var ct = app.controller;
+        if (this.time > 120) {
+            if (ct.ok || ct.cancel) {
+                app.playBGM("openingbgm");
+                this.parentScene.flare('exitgame');
+                this.exit();
+            }
+        }
+        this.time++;
+    },
+});
