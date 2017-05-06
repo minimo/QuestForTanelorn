b1d57e9b02ef90686037e1a23ae9cdb709b51424
minimo <minimo.mobile@gmail.com>
2017-05-06 18:23:55 +0900

    Update enemy algorithm.

--- src/character/enemy.knight.js
+++ src/character/enemy.knight.js
@@ -39,6 +39,14 @@ phina.define("qft.Enemy.Knight", {
         options = (options || {}).$extend({width: 20, height: 20});
         this.superInit(parentScene, options);
 
+        //武器スプライト
+        this.weapon = phina.display.Sprite("item", 24, 24).addChildTo(this).setFrameIndex(1)
+            .setAlpha(0)
+            .setOrigin(1, 1)
+            .setPosition(0, 4)
+            .setScale(-1, 1);
+        this.weapon.tweener.setUpdateType('fps');
+
         //表示用スプライト
         this.sprite = phina.display.Sprite("monster02", 24, 32).addChildTo(this);
         this.sprite.setFrameTrimming(144, 128, 72, 128);
@@ -50,6 +58,7 @@ phina.define("qft.Enemy.Knight", {
 
         this.direction = 0;
         this.stopTime = 0;
+        this.isAttack = false;
 
         this.on('damaged', e => {
             if (e.direction == 0) this.direction = 180; else this.direction = 0;
@@ -62,39 +71,28 @@ phina.define("qft.Enemy.Knight", {
         var look = this.isLookPlayer();
 
         if (this.onFloor) {
-            //崖っぷちで折り返す
-            if (this.checkMapCollision2(this.x+5, this.y+20, 5, 5) == null) {
+            //これ以上進めない場合は折り返す
+            if (this._collision[1].hit || this.checkMapCollision2(this.x+5, this.y+20, 5, 5) == null) {
                 this.direction = 180;
-            } else if (this.checkMapCollision2(this.x-5, this.y+20, 5, 5) == null) {
+            } else if (this._collision[3].hit || this.checkMapCollision2(this.x-5, this.y+20, 5, 5) == null) {
                 this.direction = 0;
             }
 
-            //壁に当たったら折り返す
-            if (this._collision[1].hit) {
-                this.direction = 180;
-            } else if (this._collision[3].hit) {
-                this.direction = 0;
-            }
-
-            //プレイヤーが近くにいたら攻撃
-            if (look && !this.isJump && dis > 64 && dis < this.eyesight && this.stopTime == 0) {
-                //火を吐く
-                var b = this.parentScene.spawnEnemy(this.x, this.y, "Bullet", {explode: true});
-                b.rotation = this.getPlayerAngle();
-                this.stopTime = 60;
-            }
-            if (look && !this.isJump && dis < 64 && this.stopTime == 0) {
-                //飛びかかる
-                this.isJump = true;
-                this.vy = -6;
-                var pl = this.parentScene.player;
-                if (this.x > pl.x) {
-                    this.direction = 180;
-                } else {
-                    this.direction = 0;
-                }
+            //攻撃
+            if (look && !this.isJump && dis < 48 && this.stopTime == 0 && !this.isAttack) {
+                var that = this;
+                this.weapon.tweener.clear()
+                    .set({rotation: 270, alpha: 1.0})
+                    .to({rotation: 430}, 6)
+                    .fadeOut(1)
+                    .call(function() {
+                        that.isAttack = false;
+                    });
+                this.isAttack = true;
+                this.stopTime = 30;
             }
         }
+
         if (this.onFloor || this.isJump) {
             if (this.direction == 0) {
                 this.vx = 1;
@@ -102,21 +100,16 @@ phina.define("qft.Enemy.Knight", {
                 this.vx = -1;
             }
         }
+
         if (look) {
             this.vx *= 3;
             this.flare('balloon', {pattern: "!"});
         } else {
-            if (dis < 256) {
-                this.flare('balloon', {pattern: "?"});
-            } else {
-                this.flare('balloonerace');
-            }
+            this.flare('balloonerace');
         }
 
+
         this.stopTime--;
-        if (this.stopTime > 0) {
-            this.vx = 0;
-        }
         if (this.stopTime < 0) {
             this.stopTime = 0;
         }
