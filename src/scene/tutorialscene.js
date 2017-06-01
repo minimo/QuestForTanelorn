dbaf759e713e6cc606feea3a28e4753254613e92
minimo <minimo.mobile@gmail.com>
2017-06-01 19:19:11 +0900

    Update tutorial scene.

--- src/scene/tutorialscene.js
+++ src/scene/tutorialscene.js
@@ -8,32 +8,301 @@
 phina.define("qft.TutorialScene", {
     superClass: "phina.display.DisplayScene",
 
-    //進行
-    seq: 0,
+    //スクリーンのマップ外への移動制限フラグ
+    limitWidth: false,
+    limitHeight: true,
 
-    init: function(currentScene) {
+    //プレイヤースクリーン中央固定フラグ
+    centerPlayer: true,
+
+    init: function(options) {
         this.superInit({width: SC_W, height: SC_H});
-        this.currentScene = currentScene;
 
         //バックグラウンド
         var param = {
-            width: SC_W,
-            height: SC_H,
-            fill: "rgba(0,0,0,1)",
-            stroke: "rgba(0,0,0,1)",
+            width:SC_W,
+            height:SC_H,
+            fill: 'black',
+            stroke: false,
             backgroundColor: 'transparent',
         };
-        this.bg = phina.display.RectangleShape(param).addChildTo(this).setPosition(SC_W*0.5, SC_H*0.5)
+        this.bg = phina.display.RectangleShape(param)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+        this.bg.tweener.setUpdateType('fps');
 
-        //イメージ表示用レイヤ
-        this.imageLayer = phina.display.DisplayElement().addChildTo(this);
+        //背景
+        this.backgroundImage = phina.display.Sprite("background").addChildTo(this).setPosition(0, SC_H*0.5).setOrigin(0, 0.5);
 
-        //フォアグラウンド
-        this.fg = phina.display.RectangleShape(param).addChildTo(this).setPosition(SC_W*0.5, SC_H*0.5)
-        this.fg.alpha = 0;
+        //管理用基準レイヤ
+        this.baseLayer = phina.display.DisplayElement().addChildTo(this);
+
+        //プレイヤーキャラクタ
+        this.player = qft.Player(this);
+
+       //スクリーン初期化
+        this.setupScreen();
+
+        //ステージ情報初期化
+        this.setupStage();
+
+        this.fg = phina.display.RectangleShape(param)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+        this.fg.tweener.setUpdateType('fps').clear().fadeOut(30);
+
+        //バーチャルパッドの可視化
+//        app.virtualPad.addChildTo(this);
+
+        app.volumeBGM = 0.5;
+        app.volumeSE = 0.2;
+
+        //メッセージ表示
+        var labelParam = {
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 1,
+
+            fontFamily: "Orbitron",
+            align: "center",
+            baseline: "middle",
+            fontSize: 16,
+            fontWeight: ''
+        };
+        var that = this;
+        this.messageStack = [];
+        this.eventMessage = phina.display.Label({text: ""}.$safe(labelParam))
+            .setPosition(SC_W*0.5, SC_H*0.2)
+            .addChildTo(this);
+        this.eventMessage.alpha = 0;
+        this.eventMessage.tweener.clear().setUpdateType('fps');
+        this.eventMessage.nextOk = true;
+        this.eventMessage.update = function() {
+            if (this.nextOk && that.messageStack.length > 0) {
+                this.nextOk = false;
+                var msg = that.messageStack[0];
+                this.tweener.clear()
+                    .call(function(){
+                        this.text = msg.text;
+                    }.bind(this))
+                    .fadeIn(15).wait(90).fadeOut(15)
+                    .call(function(){
+                        this.nextOk = true;
+                        that.messageStack.splice(0, 1);
+                    }.bind(this))
+            }
+        }
+
+        this.time = 0;
     },
 
-    update: function() {
+    update: function(app) {
+        if (this.time == 15) this.player.isControl = true;
+
+        //スクリーン表示位置をプレイヤー中心になる様に調整
+        if (this.centerPlayer) {
+            var map = this.mapLayer.map;
+            this.mapLayer.x = SC_W*0.5-this.player.x;
+            this.mapLayer.y = SC_H*0.5-this.player.y;
+            if (this.limitHeight) {
+                if (this.mapLayer.y > 0) this.mapLayer.y = 0;
+                if (this.mapLayer.y < -(map.height-SC_H)) this.mapLayer.y = -(map.height-SC_H);
+            }
+            if (this.limitWidth) {
+                if (this.mapLayer.x > 0) this.mapLayer.x = 0;
+                if (this.mapLayer.x < -(map.width-SC_W)) this.mapLayer.x = -(map.width-SC_W);
+            }
+
+            //スクリーン座標
+            this.screenX = -this.mapLayer.x;
+            this.screenY = -this.mapLayer.y;
+
+            //バックグラウンドのX座標を全体の割合から計算
+            this.backgroundImage.x = -Math.floor(this.backgroundImage.width * (this.player.x / map.width)*0.01);
+        }
+
+        this.time++;
+    },
+
+    //敵キャラクタ投入
+    spawnEnemy: function(x, y, name, options) {
+        return qft.Enemy[name](this, options).addChildTo(this.enemyLayer).setPosition(x, y);
+    },
+
+    //アイテム投入
+    spawnItem: function(x, y, options) {
+        return qft.Item(this, options).addChildTo(this.objLayer).setPosition(x, y);
+    },
+
+    //アイテムボックス投入
+    spawnItemBox: function(x, y, options) {
+        return qft.ItemBox(this, options).addChildTo(this.objLayer).setPosition(x, y);
+    },
+
+    //エフェクト投入
+    spawnEffect: function(x, y, options) {
+        return qft.Effect(this, options).addChildTo(this.objLayer).setPosition(x, y);
+    },
+
+    //メッセージ投入
+    spawnMessage: function(text, between, waitFrame) {
+        between = between || 32;
+        waitFrame = waitFrame || 30;
+        var arrayText = text.split("");
+        var labelParam = {
+            fill: "white",
+            stroke: "blue",
+            strokeWidth: 2,
+
+            fontFamily: "Orbitron",
+            align: "center",
+            baseline: "middle",
+            fontSize: 32,
+            fontWeight: ''
+        };
+        var n = 0;
+        var x = SC_W * 0.5 - (arrayText.length * between * 0.5);
+        arrayText.forEach(function(e) {
+            if (e == " ") {
+                x += between;
+                return;
+            }
+            var lb = phina.display.Label({text: e}.$safe(labelParam)).setPosition(x, SC_H*0.5+32).setScale(0, 1).addChildTo(this);
+            lb.tweener.clear().setUpdateType('fps')
+                .set({alpha: 0})
+                .wait(n)
+                .to({scaleX: 1.0, alpha: 1, y: SC_H*0.5}, 15, "easeOutSine")
+                .wait(waitFrame)
+                .to({scaleX: 0.0, y: SC_H*0.5-32}, 10, "easeOutSine")
+                .call(function(){this.remove()}.bind(lb));
+            x += between;
+            n += 5;
+        }.bind(this));
+    },
+
+    //イベントメッセージ投入
+    spawnEventMessage: function(id, text) {
+        if (!id || !text) return;
+        //メッセージスタック内に同一ＩＤがある場合は投入しない
+        for (var i = 0; i < this.messageStack.length; i++) {
+            if (this.messageStack[i].id == id) return;
+        }
+        if (text instanceof Array) {
+            text.forEach(function(str) {
+                this.messageStack.push({id: id, text: str});
+            }.bind(this));
+        } else {
+            this.messageStack.push({id: id, text: text});
+        }
+    },
+
+    //スクリーン初期化
+    setupScreen: function() {
+        var that = this;
+        var labelParam = {
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 1,
+
+            fontFamily: "UbuntuMono",
+            align: "left",
+            baseline: "middle",
+            fontSize: 20,
+            fontWeight: ''
+        };
+
+        //体力ゲージ
+        phina.display.Label({text: "LIFE"}.$safe(labelParam)).addChildTo(this).setPosition(0, 10);
+        var options = {
+            width:  256,
+            height: 5,
+            backgroundColor: 'transparent',
+            fill: 'red',
+            stroke: 'white',
+            strokeWidth: 2,
+            gaugeColor: 'lime',
+            cornerRadius: 0,
+            value: this.player.hp,
+            maxValue: this.player.hp,
+        };
+        this.lifeGauge = phina.ui.Gauge(options).addChildTo(this).setOrigin(0, 0.5).setPosition(40, 10);
+        this.lifeGauge.update = function() {
+            this.value = that.player.hp;
+        };
+
+        //スコア表示
+        this.scoreLabel = phina.display.Label({text:"SCORE:", align: "right"}.$safe(labelParam)).addChildTo(this).setPosition(SC_W, 10);
+        this.scoreLabel.score = 0;
+        this.scoreLabel.s = 0;
+        this.scoreLabel.update = function(e) {
+            if (e.ticker.frame % 10 == 0) {
+                this.s = ~~((that.totalScore-this.score)/7);
+                if (this.s < 3) this.s = 3;
+                if (this.s > 7777) this.s = 7777;
+            }
+            this.score += this.s;
+            if (this.score > that.totalScore) this.score = that.totalScore;
+
+            this.text = "SCORE "+this.score.comma();
+        }
+
+        //制限時間表示
+        var tl = phina.display.Label({text: "TIME:", align: "right"}.$safe(labelParam)).addChildTo(this).setPosition(SC_W, 30);
+        tl.update = function() {
+            this.text = "TIME:"+Math.floor(that.timeLimit/30);
+            if (that.timeLimit == 0) {
+                this.fill = 'red';
+            } else {
+                this.fill = 'white';
+            }
+        }
+
+        //プレイヤー装備武器表示
+        this.playerWeapon = qft.PlayerWeapon(this.player).addChildTo(this).setPosition(SC_W-30, SC_H-30);
+    },
+
+    //マップ情報の初期化
+    setupStage: function() {
+
+        this.baseLayer.removeChildren();
+
+        this.stageController = qft.Stage999(this);
+        this.switchMap(this.stageController.mapLayer[0]);
+
+        //経過時間初期化
+        this.time = 0;
+
+        //タイムリミット設定
+        this.timeLimit = this.stageController.timeLimit;
+
+        //プレイヤー設定
+        this.player.isControl = false;
+        this.player.isMuteki = false;
+        this.player.alpha = 1.0;
+
+        this.stageController.playBGM();
+    },
+
+    //マップレイヤの切替
+    switchMap: function(layer, x, y) {
+        if (this.mapLayer) this.mapLayer.remove();
+        this.mapLayer = layer;
+        this.mapLayer.addChildTo(this.baseLayer);
+        this.map = layer.map;
+
+        this.backgroundLayer = layer.backgroundLayer;
+        this.foregroundLayer = layer.foregroundLayer;
+        this.collisionLayer = layer.collisionLayer;
+        this.enemyLayer = layer.enemyLayer;
+        this.playerLayer = layer.playerLayer;
+        this.objLayer = layer.objLayer;
+        this.mapImageLayer = layer.mapImageLayer;
+        this.effectLayer = layer.effectLayer;
+
+        this.player.remove();
+        this.player.addChildTo(layer.playerLayer);
+        if (x) {
+            this.player.setPosition(x, y);
+        }
     },
 });
-
