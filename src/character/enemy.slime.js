305a0b7e6d6c2b3f81d152dba9c86320f573d08c
minimo <minimo.mobile@gmail.com>
2017-06-13 17:41:24 +0900

    Update enemy.

--- src/character/enemy.slime.js
+++ src/character/enemy.slime.js
@@ -45,7 +45,9 @@ phina.define("qft.Enemy.Slime", {
 
         //表示用スプライト
         this.sprite = phina.display.Sprite("monster01", 24, 32).addChildTo(this);
-        this.sprite.setFrameTrimming(Math.min(this.level, 4) * 72, 256, 72, 128);
+        this.sprite.setFrameTrimming(Math.min(this.level, 4) * 72, 256, 72, 128)
+            .setScale(1 + Math.min(this.level, 4) * 0.2)
+            .setPosition(0, Math.min(this.level, 4) * -2)
         this.hp += this.level * 10;
         this.power += this.level * 5;
 
--- src/character/enemy.snake.js
+++ src/character/enemy.snake.js
@@ -42,8 +42,8 @@ phina.define("qft.Enemy.Snake", {
         //表示用スプライト
         this.sprite = phina.display.Sprite("monster01", 24, 32).addChildTo(this);
         this.sprite.setFrameTrimming(Math.min(this.level, 4) * 72, 384, 72, 128)
-            .setScale(1 + this.level * 0.2)
-            .setPosition(0, this.level * -2)
+            .setScale(1 + Math.min(this.level, 4) * 0.1)
+            .setPosition(0, Math.min(this.level, 4) * -1)
         this.hp += this.level * 10;
         this.power += this.level * 5;
 
