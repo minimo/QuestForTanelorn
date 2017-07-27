0f01ae95bbcd8020c804a3728d56fb0846d01d74
minimo <minimo.mobile@gmail.com>
2017-07-27 19:07:17 +0900

    Update true ending.

--- src/scene/endingscene.true.js
+++ src/scene/endingscene.true.js
@@ -14,6 +14,15 @@ phina.define("qft.EndingScene.true", {
         this.superInit({width: SC_W, height: SC_H});
         this.parentScene = parentScene;
 
+        this.text = [
+            "数多の苦難を乗り越え",
+            "あなたは遂に楽園への入り口へと到達した",
+            "扉の向こうには階段があり",
+            "その階段は遥か天上へと続いている",
+            "階段を昇りますか？",
+            "",
+        ];
+
         //バックグラウンド
         var param = {
             width:SC_W,
@@ -29,9 +38,20 @@ phina.define("qft.EndingScene.true", {
             .set({alpha: 0})
             .to({alpha: 0.5}, 1000);
 
-        app.playBGM("gameover", false);
+        //イメージ表示用レイヤ
+        this.imageLayer = phina.display.DisplayElement().addChildTo(this);
+
+        //上下黒帯
+        param.height = SC_H * 0.15;
+        this.bar1 = phina.display.RectangleShape(param).addChildTo(this).setOrigin(0, 0).setPosition(0, -SC_H*0.15)
+        this.bar2 = phina.display.RectangleShape(param).addChildTo(this).setOrigin(0, 0).setPosition(0, SC_H)
 
         this.time = 0;
+
+        this.one('enterframe', () => {
+            //ＢＧＭ再生
+            app.playBGM("endingbgm");
+        };
     },
 
     update: function(app) {
