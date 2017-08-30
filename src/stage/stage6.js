2efb3a30d8e9ec07785528d6475097a68f7083bf
minimo <minimo.mobile@gmail.com>
2017-08-30 14:38:01 +0900

    Fix stage 6.

--- src/stage/stage6.js
+++ src/stage/stage6.js
@@ -38,14 +38,57 @@ phina.define("qft.Stage6", {
             this.limitHeight = true;
         });
 
-        this.add(60, function() {
+        this.bossdead1 = false;
+        this.bossdead2 = false;
+
+        //ボス１討伐
+        this.addEvent("bossdead_1", () => {
+            this.bossdead1 = true;
+            if (this.bossdead2) this.fireEvent("key");
+        });
+        //ボス２討伐
+        this.addEvent("bossdead_2", () => {
+            this.bossdead2 = true;
+            if (this.bossdead1) this.fireEvent("key");
+        });
+
+        //鍵出現
+        this.addEvent("key", () => {
+            var kx = 384;
+            var ky = 944;
+            var key = qft.Item(this.parentScene, {properties: {kind: "key"}})
+                .addChildTo(this.parentScene.mapLayer.objLayer)
+                .setPosition(kx, ky);
+            key.vy = -5;
+            this.parentScene.pauseScene = true;
+            this.parentScene.centerPlayer = false;
+            this.parentScene.camera.setPosition(this.parentScene.player.x, this.parentScene.player.y);
+            this.parentScene.camera.tweener.clear()
+                .call(function() {
+                    app.playSE("holy1");
+                })
+                .moveTo(kx, ky, 30, "easeInOutSine")
+                .wait(30)
+                .call(function() {
+                    this.parentScene.pauseScene = false;
+                    this.moveFrom.x = kx;
+                    this.moveFrom.y = ky;
+                    this.moveRaio = 0;
+                    this.moveLerp = true;
+                    this.moveToPlayer = true;
+                }.bind(this.parentScene.camera))
+                .to({moveRatio: 1}, 30, "easeInOutSine")
+                .call(function() {
+                    this.parentScene.centerPlayer = true;
+                    this.moveLerp = false;
+                }.bind(this.parentScene.camera));
         });
     },
 
     //ステージクリア条件判定
     checkStageClearCondition: function() {
         var keys = this.player.keys;
-        if (keys.length < 1) return false;
+        if (keys.length < 2) return false;
         return true;
     },
 
@@ -62,7 +105,7 @@ phina.define("qft.Stage6", {
             .wait(60)
             .call(function() {
                 pl.setAnimation("up");
-                pl.tweener.clear().moveTo(487, 271, 1000)
+                pl.tweener.clear().moveTo(119 + 18　, 448 + 80, 1000)
                     .call(function() {
                         pl.animation = false;
                     })
