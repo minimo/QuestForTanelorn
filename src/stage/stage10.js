03e82a65600193a0932e05f033904a32e5f46837
minimo <minimo.mobile@gmail.com>
2017-09-20 06:54:13 +0900

    Update ending stage.

--- src/stage/stage10.js
+++ src/stage/stage10.js
@@ -30,9 +30,6 @@ phina.define("qft.Stage9", {
 
         //初期処理
         this.add(1, function() {
-            //ステージ開始メッセージ投入
-            this.spawnMessage("STAGE 9", 24);
-
             //マップ表示設定
             this.limitWidth = true;
             this.limitHeight = true;
@@ -44,29 +41,10 @@ phina.define("qft.Stage9", {
 
     //ステージクリア条件判定
     checkStageClearCondition: function() {
-        var keys = this.player.keys;
-        if (keys.length < 1) return false;
         return true;
     },
 
     //ステージクリア処理
     stageClear: function() {
-        var pl = qft.PlayerDummy("player1").addChildTo(this.parentScene.mapLayer.playerLayer).setVisible(false);
-        pl.setAnimation("clear");
-        this.player.tweener.clear()
-            .wait(30)
-            .set({alpha: 0})
-            .call(function() {
-                pl.setPosition(this.player.x, this.player.y).setVisible(true);
-            }.bind(this))
-            .wait(60)
-            .call(function() {
-                pl.setAnimation("up");
-                pl.tweener.clear().moveTo(487, 271, 1000)
-                    .call(function() {
-                        pl.animation = false;
-                    })
-                    .fadeOut(500);
-            }.bind(this));
     },
 });
