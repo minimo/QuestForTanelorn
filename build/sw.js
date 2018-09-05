65fc230b5d5003867c54752b9eca80568fd753aa
minimo <minimo.mobile@gmail.com>
2018-09-05 20:06:16 +0900

    udpate

--- build/sw.js
+++ build/sw.js
@@ -12,8 +12,52 @@ self.addEventListener('install', function(event) {
         console.log('[Service Worker] Precaching App...');
         cache.addAll([
           '/',
-          '/qdt.js',
+          '/qft.js',
         ]);
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ...');
+  event.respondWith(
+    // キャッシュの存在チェック
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  // 最後に res を返せるように、ここでは clone() する必要がある
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+              // エラーが発生しても何もしない
+            });
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker...');
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_VERSION && key !== CACHE_DYNAMIC_VERSION) {
+            console.log('[Service Worker] Removing old cache...');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
