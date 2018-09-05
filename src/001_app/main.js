96f39b3c01852febc4a29d7034df062c860c4678
minimo <minimo.mobile@gmail.com>
2018-09-05 21:12:40 +0900

    update

--- src/001_app/main.js
+++ src/001_app/main.js
@@ -54,8 +54,13 @@ const ITEM_POTION = 27;
 let app;
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-      .then(() => console.log('Service worker registered!'));
+    navigator.serviceWorker.register('sw.js')
+      .then(() => {
+          console.log('Service worker registered!');
+      })
+      .reject(() => {
+        console.log('Service worker rejected!');
+    });
 }
 
 window.onload = function() {
